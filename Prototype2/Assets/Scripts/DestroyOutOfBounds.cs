dbb782af8c831b035083b389fbb4998ae35784fe
xavierlazaro <142627555+xavierlazaro@users.noreply.github.com>
2024-04-03 13:45:27 -0700

    made a const string for game over, removed unused libraries

--- Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30.0f;
     private float lowerBound = -10.0f;
-
+private const string gameover "GameOver";
     // Update is called once per frame
     void Update()
     {
@@ -19,7 +17,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z < lowerBound)
         {
             //Log "Game over to the console
-            Debug.Log("GameOver");
+            Debug.Log(GameOver);
             Destroy(gameObject);
         }
     }
